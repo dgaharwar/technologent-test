5881c7ea38850031853ccbc971610dad7af52e13
Deepti Gaharwar <55699056+dgaharwar@users.noreply.github.com>
2022-08-10 13:47:39 +0530

    Update install_apache.sh

--- install_apache.sh
+++ install_apache.sh
@@ -1,6 +1,10 @@
-{#! /bin/bash}
-{apt-get update}
-{apt-get install -y apache2}
-{systemctl start apache2}
-{systemctl enable apache2}
-{echo "<h1>Deployed via Terraform</h1>" || tee /var/www/html/index.html}
+user_data = <<-EOF
+
+#! /bin/bash
+apt-get update
+apt-get install -y apache2
+systemctl start apache2
+systemctl enable apache2
+echo "<h1>Deployed via Terraform</h1>" | tee /var/www/html/index.html
+
+EOF
